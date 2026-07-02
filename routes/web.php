5b4fffabcde51b7f0e4ed8dc5acc99ecf43ccd59
Piergiuseppe Meo <piergiuseppe.meo@icar.cnr.it>
2026-07-02 21:26:51 +0200

    manuale

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/admin');
 
+Route::get('/admin/manuale', function () {
+    return response()->download(public_path('docs/Manuale Proforma.pdf'), 'Manuale Proforma.pdf');
+})->name('filament.admin.pages.manuale');
+
 /*
  * Route::get('/', function () {
  *     return view('welcome');
